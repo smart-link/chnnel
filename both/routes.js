efa1f948789de4f098a110ce18d1ea3e6475cb7c
Jido Park <parkmap@gmail.com>
2015-07-18 14:45:20 +0900

    change some template

--- both/routes.js
+++ both/routes.js
@@ -1,5 +1,5 @@
 Router.route('/', {
-  name: 'trending'
+  name: 'channel'
 });
 
 Router.route('/recent', {
---
+++ client/templates/channel/channel.js
@@ -0,0 +1,21 @@
+Template.channel.created = function () {
+  this.autorun(function () {
+    this.subscription = Meteor.subscribe('products');
+  }.bind(this));
+};
+
+Template.channel.rendered = function () {
+  this.autorun(function () {
+    if (!this.subscription.ready()) {
+      IonLoading.show();
+    } else {
+      IonLoading.hide();
+    }
+  }.bind(this));
+};
+
+Template.channel.helpers({
+  products: function () {
+    return Products.find({}, {sort: {numberOfVotes: -1, name: -1}});
+  }
+});
