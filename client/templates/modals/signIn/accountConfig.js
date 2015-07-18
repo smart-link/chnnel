fa63af2274bc55501d9b4932ed0101cb1128f47b
unamedkr <unamed.kr@gmail.com>
2015-07-18 14:45:22 +0900

    after signin/signup

--- client/templates/modals/signIn/accountConfig.js
+++ client/templates/modals/signIn/accountConfig.js
@@ -1,6 +1,22 @@
+var onSubmitHook = function(error, state){
+  if (!error) {
+    if (state === "signIn") {
+      console.log('signUp signIn');
+      IonModal.close();
+      
+    }
+    if (state === "signUp") {
+      console.log('signUp success');
+      IonModal.close();
+      
+    }
+  }
+};
+
 AccountsTemplates.configure({
   negativeValidation: false,
   negativeFeedback: false,
   positiveValidation: false,
   positiveFeedback: false,
+  onSubmitHook: onSubmitHook
 });
--- client/templates/modals/signIn/signIn.js
+++ client/templates/modals/signIn/signIn.js
@@ -1,10 +1,21 @@
-Template.signIn.events({
-  'click [data-action=sign-in]': function (event, template) {
-    console.log('test');
-    IonModal.close();
-  },
-  'click [data-action=sign-up]': function (event, template) {
-    IonModal.close();
+var onSubmitHook = function(error, state){
+  if (!error) {
+    console.log(state)
+    if (state === "signIn") {
+      IonModal.close();
+      Router.go('/');
+    }
+    if (state === "signUp") {
+      IonModal.close();
+      Router.go('/');
+    }
   }
-});
+};
 
+AccountsTemplates.configure({
+  negativeValidation: false,
+  negativeFeedback: false,
+  positiveValidation: false,
+  positiveFeedback: false,
+  onSubmitHook: onSubmitHook
+});
