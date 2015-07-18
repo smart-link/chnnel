a92c0c84cddcfd43ed45cb6a6c5483a0bcd93427
Jido Park <parkmap@gmail.com>
2015-07-18 15:47:33 +0900

    change something

--- both/routes.js
+++ both/routes.js
@@ -10,6 +10,11 @@ Router.route('/products/:_id', {
   name: 'products.show'
 });
 
+
+Router.route('/channels/:_id', {
+  name: 'channels.show'
+});
+
 Router.route('/users/:_id', {
   name: 'users.show'
 });
